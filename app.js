b62a47339bb7ccd9b324c09ff19388697c715ef2
Mitch Crowe <crowe.mitch@gmail.com>
2018-01-23 21:12:32 -0800

    Initial commit.

--- app.js
+++ app.js
@@ -1,17 +1,58 @@
-state.counter = 1
+state.squadrons = [
+  {
+    name: "Brad's Goons",
+    number: 5,
+    faction: "Rebel",
+  },
+  {
+    name: "Mitch's Mighty Minions",
+    number: 7,
+    faction: "Imperial"
+  },
+  {
+    name: "The Rebel Alliance",
+    number: 1,
+    faction: "Rebel"
+  },
+  {
+    name: "The Empirials Strikers",
+    number: 50,
+    faction: "Imperial"
+  }
+]
 
-helper('magicCalculation', x => {
-  return x * 42
+state.newSquadron = {}
+
+on('click', '.squadron-link', event => {
+  let id = parseInt(event.data.id)
+  show('squadron', { id: id })
 })
 
-on('click', '.show-b', () => {
-  state.counter += 1
-  show('b', {value: 9})
+show('home')
+
+on('click', '.return-button', () => {
+  show('home')
 })
 
-on('click', '.show-a', () => {
-  state.counter += 1
-  show('a')
+on('click', '.new-squad', () => {
+  show('new')
 })
 
-show('a')
+on('click', '.save', () => {
+  state.squadrons.push(state.newSquadron)
+  state.newSquadron = {}
+  show('home')
+})
+
+// Remember the new squadron name when we type.
+on('change', '.squadron-name', event => {
+  state.newSquadron.name = event.value
+})
+
+on('change', '.squadron-number', event => {
+  state.newSquadron.number = parseInt(event.value)
+})
+
+on('change', '.squadron-faction', event => {
+  state.newSquadron.faction = event.value
+})
